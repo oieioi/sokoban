50297f0bdc501641747dc14bbbb283c35c9cb068
oieioi <atsuinatsu.samuifuyu@gmail.com>
2018-07-23 18:08:42 +0900

    Generate questions

---
+++ db/migrate/20180723090207_create_questions.rb
@@ -0,0 +1,15 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.string :name, null: false, comment: 'title'
+      t.string :description, null: true, comment: 'ヒント'
+      t.string :map, null: false, comment: 'マップ'
+      t.references :use, foreign_key: true, null: false, comment: '出題者'
+      t.boolean :published, default: false, null: false, comment: '公開済みか'
+      t.boolean :solved, default: false, null: false, comment: '解かれているか'
+      t.string :answer, null: true, comment: '模範回答'
+
+      t.timestamps
+    end
+  end
+end
