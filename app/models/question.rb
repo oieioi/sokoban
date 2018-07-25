8bc3145f62ee457ab1955d8a9396f7e6396fb4dc
oieioi <atsuinatsu.samuifuyu@gmail.com>
2018-07-25 21:19:43 +0900

    Player move(up only)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
-  validates :map, presence: true, format: { with: %r[\A(enpbrw)+\Z]}
+  validates :map, presence: true
+  validate :validate_map
 
   # TODO enum
   TILES = {
@@ -14,7 +15,7 @@ class Question < ApplicationRecord
   }.freeze
 
   def map2array
-    map.split('')
+    @arrayed_map ||= map.split('')
       .reduce([[]]) { |result, string|
         if string == TILES[:new_line]
           result << []
@@ -25,26 +26,87 @@ class Question < ApplicationRecord
       }
   end
 
-  def tile(x, y)
+  def point(x:, y:, **)
     line = map2array[y]
-    line&[x]
+    line.try(:[], x)
+  end
+
+  # 上の座標を返す
+  def up_point(x:, y:, **)
+    {x: x, y: y - 1}
+  end
+
+  def down_point(x:, y:)
+    {x: x, y: y + 1}
+  end
+
+  def right_point(x:, y:)
+    {x: x + 1, y: y}
+  end
+
+  def left_point(x:, y:)
+    {x: x - 1, y: y}
   end
 
   def player_point
+    x = nil
+    y = map2array.index { |line| x = line.index(TILES[:player]) }
+    {x: x, y: y}
   end
 
-  def player_move_up
+  def player_move_up!
+    if player_can_up?
+      old_player = {movable: TILES[:empty]}.merge player_point
+      new_player = {movable: TILES[:player]}.merge up_point(player_point)
+      move!(old_player)
+      move!(new_player)
+    elsif player_can_push_up?
+      current_player = {movable: TILES[:empty]}.merge player_point
+      box = {movable: TILES[:player]}.merge up_point(current_player)
+      new_box = {movable: TILES[:box]}.merge up_point(box)
+      move!(current_player)
+      move!(box)
+      move!(new_box)
+    else
+      nil
+    end
   end
 
-  def player_can_move_up?
+  def move!(x:, y:, movable:)
+    map = map2array
+    map[y][x] = movable
+    map
   end
 
-  #def player_move_down
-  #end
+  def show
+    map2array.each {|line|
+      line.each {|tile| print tile}
+      puts ''
+    }
+    nil
+  end
 
-  #def player_move_left
-  #end
+  def player_can_up?
+    tile = point(up_point(player_point))
+    return tile == TILES[:empty]
+  end
 
-  #def player_move_right
-  #end
+  # box がある場合の判定
+  def player_can_push_up?
+    tile = point(up_point(player_point))
+    # boxの上がempty, right_placeならok
+    if tile == TILES[:box]
+      next_tile = point(up_point(up_point(player_point)))
+      result = [TILES[:empty], TILES[:right_place]].include?(next_tile)
+      return result
+    end
+    false
+  end
+
+  private
+
+  def validate_map
+    # 
+    return true
+  end
 end
